5e468fdeef940ce20cae5ee9ca1c1a6c0a439bc6
Tyuiu.KulkoDA@outlook.com <Tyuiu.KulkoDA@outlook.com>
2024-10-03 12:09:47 +0500

    Task2

---
+++ Tyuiu.KulkoDA.Sprint2.Task2.V3.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+using Tyuiu.KulkoDA.Sprint2.Task2.V3.Lib;
+namespace Tyuiu.KulkoDA.Sprint2.Task2.V3.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int x = 9;
+            int y = 3;
+            bool res = ds.CheckDotInShadedArea(x, y);
+            Assert.AreEqual(true, res);
+        }
+    }
+}
---
+++ Tyuiu.KulkoDA.Sprint2.Task2.V3/Program.cs
@@ -0,0 +1,36 @@
+﻿using Tyuiu.KulkoDA.Sprint2.Task2.V3.Lib;
+namespace Tyuiu.KulkoDA.Sprint2.Task2.V3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #2 | Выполнила: Кулько Д. А. | ИИПб-24-2";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Оператор if - полная и короткая форма записи                      *");
+            Console.WriteLine("* Задание #2                                                              *");
+            Console.WriteLine("* Вариант #3                                                              *");
+            Console.WriteLine("* Выполнила: Кулько Дарья Андреевна | ИИПб-24-2                           *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая запрашивает целые значения с клавиатуры и   *");
+            Console.WriteLine("* вычисляет находится ли точка с координатами XY в заштрихованной области.*");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*ИСХОДНЫЕ ДАННЫЕ:                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Рисунок                                                                 *");
+            Console.WriteLine("Введите значение X:");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите значение Y:");
+            int y = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*РЕЗУЛЬТАТ:                                                               *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine(ds.CheckDotInShadedArea(x, y));
+            Console.ReadLine();
+        }
+    }
+}
