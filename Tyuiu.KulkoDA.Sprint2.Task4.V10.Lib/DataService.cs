e1356f5b5061cacbf60339611a214885f9455aac
Tyuiu.KulkoDA@outlook.com <Tyuiu.KulkoDA@outlook.com>
2024-10-03 13:40:26 +0500

    Task4

---
+++ Tyuiu.KulkoDA.Sprint2.Task4.V10.Lib/DataService.cs
@@ -0,0 +1,12 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+namespace Tyuiu.KulkoDA.Sprint2.Task4.V10.Lib
+{
+    public class DataService : ISprint2Task4V10
+    {
+        public double Calculate(double x, double y)
+        {
+            var res = x*2<y/3?Math.Pow((1+3/y*y),x) : y+10*x-(5/x);
+            return Math.Round(res,3);
+        }
+    }
+}
---
+++ Tyuiu.KulkoDA.Sprint2.Task4.V10.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+using Tyuiu.KulkoDA.Sprint2.Task4.V10.Lib;
+namespace Tyuiu.KulkoDA.Sprint2.Task4.V10.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            double x = 1;
+            double y = 1;
+            var res = ds.Calculate(x, y);
+            Assert.AreEqual(6, res);
+        }
+    }
+}
