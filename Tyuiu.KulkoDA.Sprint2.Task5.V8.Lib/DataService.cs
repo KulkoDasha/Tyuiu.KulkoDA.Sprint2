eb9e3892c9d008a7285bf2b933e9733063e0dd56
Tyuiu.KulkoDA@outlook.com <Tyuiu.KulkoDA@outlook.com>
2024-10-12 13:45:08 +0500

    Task5.1111

--- Tyuiu.KulkoDA.Sprint2.Task5.V8.Lib/DataService.cs
+++ Tyuiu.KulkoDA.Sprint2.Task5.V8.Lib/DataService.cs
@@ -6,83 +6,83 @@ namespace Tyuiu.KulkoDA.Sprint2.Task5.V8.Lib
         public string FindDateOfPreviousDay(int m, int n)
         {
             string res;
-            switch (m)
+            switch (n)
             {
                 case 1:
-                    if (n >= 2)
-                        res =(n - 1) + "."+1;
+                    if (m >= 2)
+                        res =(m - 1) + "."+ 01;
                     else
                         res = 31+"."+01;
                     break;
 
                 case 2:
-                    if (n >= 2)
-                        res = (n - 1) + "." + 2;
+                    if (m >= 2)
+                        res = (m - 1) + "." + 02;
                     else
                         res = 31 + "." + 01;
                     break;
                 case 3:
-                    if (n >= 2)
-                        res = (n - 1) + "." + 3;
+                    if (m >= 2)
+                        res = (m - 1) + "." + 03;
                     else
                         res = 28 + "." + 02;
                     break;
                 case 4:
-                    if (n >= 2)
-                        res = (n - 1) + "." + 4;
+                    if (m >= 2)
+                        res = (m - 1) + "." + 04;
                     else
                         res = 31 + "." + 03;
                     break;
                 case 5:
-                    if (n >= 2)
-                        res = (n - 1) + "." + 5;
+                    if (m >= 2)
+                        res = (m - 1) + "." + 05;
                     else
                         res = 30 + "." + 04;
                     break;
                 case 6:
-                    if (n >= 2)
-                        res = (n - 1) + "." + 6;
+                    if (m >= 2)
+                        res = (m - 1) + "." + 06;
                     else
                         res = 31 + "." + 05;
                     break;
                 case 7:
-                    if (n >= 2)
-                        res = (n - 1) + "." + 7;
+                    if (m >= 2)
+                        res = (m - 1) + "." + 07;
                     else
                         res = 30 + "." + 06;
                     break;
                 case 8:
-                    if (n >= 2)
-                        res = ((n - 1) + "." + 8);
+                    if (m >= 2)
+                        res = ((m - 1) + "." + 08);
                     else
                         res = 31 + "." + 07;
                     break;
                 case 9:
-                    if (n >= 2)
-                        res = (n - 1) + "." + 9;
+                    if (m >= 2)
+                        res = (m - 1) + "." + 09;
                     else
                         res = 31 + "." + 08;
                     break;
                 case 10:
-                    if (n >= 2)
-                        res = (n - 1) + "." + 10;
+                    if (m >= 2)
+                        res = (m - 1) + "." + 10;
                     else
                         res = 30 + "." + 09;
                     break;
                 case 11:
-                    if (n >= 2)
-                        res = (n - 1) + "." + 11;
+                    if (m >= 2)
+                        res = (m - 1) + "." + 11;
                     else
                        res = 31 + "." + 10;
                     break;
                 case 12:
-                    if (n >= 2)
-                        res = ((n - 1) + "." + 12);
+                    if (m >= 2)
+                        res = ((m - 1) + "." + 12);
                     else
                         res = 30+"."+11;
                     break;
                 default:
-                    throw new ArgumentException($"Месяц должен быть от 1 до 12. Значение{m}");
+                    throw new ArgumentException($"Месяц должен быть от 1 до 12. Значение{n}");
                     
                 
             }
